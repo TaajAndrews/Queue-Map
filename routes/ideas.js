f810a358a5e03213ebaf01369eb799c27b2ba04e
Taaj Andrews <taajandrews@gmail.com>
2023-07-13 14:40:12 -0400

    Add middleware auth to idea routes

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,10 +1,25 @@
 const router = require("express").Router()
 const controller = require("../controllers/ideas")
-// const middleware = require("../middleware")
+const middleware = require("../middleware")
 
-router.get("/", controller.index)
-router.post("/", controller.create)
-router.put("/:idea_id", controller.update)
-router.delete("/:idea_id", controller.deleteIdea)
+router.get("/", middleware.stripToken, middleware.verifyToken, controller.index)
+router.post(
+  "/",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.create
+)
+router.put(
+  "/:idea_id",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.update
+)
+router.delete(
+  "/:idea_id",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.deleteIdea
+)
 
 module.exports = router
