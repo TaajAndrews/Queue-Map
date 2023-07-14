906b5313d59aa9e41e74a4f372e53812a0b4ded7
Taaj Andrews <taajandrews@gmail.com>
2023-07-14 02:29:39 -0400

    Set up conditional rendering based on user state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import "./styles/App.css"
+import NewIdeaPage from "./pages/NewIdeaPage/NewIdeaPage"
+import AuthPage from "./pages/AuthPage/AuthPage"
+import IdeaIndexPage from "./pages/IdeaIndexPage/IdeaIndexPage"
 
 const App = () => {
   const [user, setUser] = useState(null)
 
-  return <main className="App">App</main>
+  return <main className="App">{user ? <NewIdeaPage /> : <AuthPage />}</main>
 }
 
 export default App
