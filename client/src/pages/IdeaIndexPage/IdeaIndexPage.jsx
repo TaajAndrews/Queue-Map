7726dc604d7044c8263d9e51186de55bc1784137
Taaj Andrews <taajandrews@gmail.com>
2023-07-14 02:12:28 -0400

    Create initial Idea and Auth page components

---
+++ client/src/pages/IdeaIndexPage/IdeaIndexPage.jsx
@@ -0,0 +1,11 @@
+// import { useEffect, useState } from "react"
+
+const IdeaIndexPage = () => {
+  return (
+    <div>
+      <h1>Idea Index Page</h1>
+    </div>
+  )
+}
+
+export default IdeaIndexPage
---
+++ client/src/pages/NewIdeaPage/NewIdeaPage.jsx
@@ -0,0 +1,11 @@
+// import { useEffect, useState } from "react"
+
+const NewIdeaPage = () => {
+  return (
+    <div>
+      <h1>New Idea Page</h1>
+    </div>
+  )
+}
+
+export default NewIdeaPage
