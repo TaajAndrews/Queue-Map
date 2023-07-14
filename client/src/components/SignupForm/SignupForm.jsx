d09bf0b1d0fc13ba067bf3638b8e94e354b9833e
Taaj Andrews <taajandrews@gmail.com>
2023-07-14 14:20:39 -0400

    Update Sign Up Form

--- client/src/components/SignupForm/SignupForm.jsx
+++ client/src/components/SignupForm/SignupForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import axios from "axios"
 
 const SignupForm = () => {
   const initialState = {
@@ -10,19 +11,51 @@ const SignupForm = () => {
 
   const [formState, setFormState] = useState(initialState)
 
-  // const handleChange = (event) =>
-  // const disable =
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    await axios.post("http://localhost:3001/auth/signup", formState)
+    setFormState(initialState)
+  }
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+    // const disable =
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="username">Username:</label>
-      <input type="text" id="username" />
+      <input
+        type="text"
+        id="username"
+        onChange={handleChange}
+        value={formState.username}
+        required
+      />
       <label htmlFor="email">Email:</label>
-      <input type="email" id="email" />
+      <input
+        type="email"
+        id="email"
+        onChange={handleChange}
+        value={formState.email}
+        required
+      />
       <label htmlFor="password">Password:</label>
-      <input type="password" id="password" />
+      <input
+        type="password"
+        id="password"
+        onChange={handleChange}
+        value={formState.password}
+        required
+      />
       <label htmlFor="confirmPassword">Confirm Password:</label>
-      <input type="password" id="confirm-password" />
+      <input
+        type="password"
+        id="confirm-password"
+        onChange={handleChange}
+        value={formState.confirmPassword}
+        required
+      />
       <button type="submit">Send</button>
     </form>
   )
