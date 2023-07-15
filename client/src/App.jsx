5f3d975214ceddfb7a43b290e711137c55bed295
Taaj Andrews <taajandrews@gmail.com>
2023-07-15 13:02:56 -0400

    Update App.jsx Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,42 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
-import axios from "axios"
+import { CheckSession } from "./services/Auth"
+import Nav from "./components/Nav"
 import "./styles/App.css"
-import NewIdeaPage from "./pages/NewIdeaPage"
-import AuthPage from "./pages/SignupPage"
-import IdeaIndexPage from "./pages/IdeaIndexPage"
-import Header from "./components/Header"
+import SignupPage from "./pages/SignupPage"
+import SigninPage from "./pages/SigninPage"
+import LandingPage from "./pages/LandingPage"
 
 const App = () => {
   const [user, setUser] = useState(null)
 
+  const handleLogOut = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div>
+      <Nav user={user} handleLogOut={handleLogOut} />
       <main className="App">
-        {user ? (
-          <>
-            <Header />
-            <Routes>
-              <Route path="/ideas/new" element={<NewIdeaPage />} />
-              <Route path="/ideas" element={<IdeaIndexPage />} />
-            </Routes>
-          </>
-        ) : (
-          <AuthPage />
-        )}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signin" element={<SigninPage setUser={setUser} />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/ideas" element={<IdeaLandingPage user={user} />} />
+        </Routes>
       </main>
     </div>
   )
